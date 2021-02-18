81f7b2e4b35c25a53c7be9a3076dd6dc6f4fa7a2
Oleksandr Voloshyn <oleksandr.voloshyn@jetbrains.com>
2021-02-18 13:48:31 +0100

    Extract editor to a separate file
    Rename RunConfiguration file to RunConfigurationFactory

--- src/main/kotlin/org/jetbrains/tinygoplugin/runconfig/TinyGoFlashConfiguration.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/runconfig/TinyGoFlashConfiguration.kt
@@ -8,90 +8,15 @@ import com.goide.util.GoExecutor
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.RuntimeConfigurationException
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.observable.properties.GraphPropertyImpl.Companion.graphProperty
-import com.intellij.openapi.observable.properties.PropertyGraph
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.options.SettingsEditorGroup
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.ui.layout.GrowPolicy
-import com.intellij.ui.layout.panel
-import com.intellij.util.io.exists
 import org.jetbrains.tinygoplugin.configuration.GarbageCollector
 import org.jetbrains.tinygoplugin.configuration.Scheduler
 import org.jetbrains.tinygoplugin.configuration.TinyGoConfiguration
-import java.nio.file.Path
 import java.nio.file.Paths
-import javax.swing.JComponent
-
-class TinyGoConfigurationEditor(defaultConfiguration: TinyGoFlashConfiguration) :
-    SettingsEditor<TinyGoFlashConfiguration>() {
-
-    private val propertyGraph = PropertyGraph()
-    private val sdkProperty = propertyGraph.graphProperty { "" }
-    private val gcProperty = propertyGraph.graphProperty { GarbageCollector.AUTO_DETECT }
-    private val schedulerProperty = propertyGraph.graphProperty { Scheduler.AUTO_DETECT }
-    private val targetProperty = propertyGraph.graphProperty { "" }
-    private val tinyGoArguments = propertyGraph.graphProperty { "" }
-    private val main = propertyGraph.graphProperty { "" }
-
-    init {
-        resetEditorFrom(defaultConfiguration)
-    }
-
-    override fun resetEditorFrom(configuration: TinyGoFlashConfiguration) {
-        sdkProperty.set(configuration.tinyGoSDKPath)
-        sdkProperty.set(configuration.tinyGoSDKPath)
-        gcProperty.set(configuration.gc)
-        schedulerProperty.set(configuration.scheduler)
-        targetProperty.set(configuration.target)
-        main.set(configuration.mainFile.path)
-        /* ktlint-disable */
-        tinyGoArguments.set(
-            listOf(
-                "flash",
-                "-target", configuration.target,
-                "-scheduler", configuration.scheduler.cmd,
-                "-gc", configuration.gc.cmd
-            ).joinToString(separator = " ")
-            /* ktlint-enable */
-        )
-    }
-
-    override fun applyEditorTo(tinyGoFlashConfiguration: TinyGoFlashConfiguration) {
-        tinyGoFlashConfiguration.cmdlineOptions = tinyGoArguments.get().split(' ').filter {
-            it.trim().isNotEmpty()
-        }.toMutableList()
-        val mainFile = Path.of(main.get())
-        if (mainFile.exists()) {
-            tinyGoFlashConfiguration.mainFile = VfsUtil.findFile(mainFile, true)!!
-        }
-    }
-
-    override fun createEditor(): JComponent {
-        return panel {
-            row("TinyGo path") {
-                textField(sdkProperty).enabled(false)
-            }
-            row("Target") {
-                textField(targetProperty).enabled(false)
-            }
-            row("Command line arguments") {
-                textField(tinyGoArguments).growPolicy(GrowPolicy.MEDIUM_TEXT)
-            }
-            row("Path to main") {
-                val fileChooserDescriptor = FileChooserDescriptor(true, false, false, false, false, false)
-                textFieldWithBrowseButton(
-                    property = main,
-                    fileChooserDescriptor = fileChooserDescriptor
-                )
-            }
-        }
-    }
-}
 
 class TinyGoRunningState(env: ExecutionEnvironment, module: Module, configuration: TinyGoFlashConfiguration) :
     GoRunningState<TinyGoFlashConfiguration>(env, module, configuration) {
