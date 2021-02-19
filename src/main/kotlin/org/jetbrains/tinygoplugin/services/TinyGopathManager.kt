74256aed979c4da55e4a0249132d21ec2e6da822
Oleksandr Voloshyn <oleksandr.voloshyn@jetbrains.com>
2021-02-19 15:03:55 +0100

    Fix root folder return if tinygoSDK is null
    Add check for missing sources folder
    Change mutable collections to immutable
    Fix arguments naming

--- src/main/kotlin/org/jetbrains/tinygoplugin/services/TinyGopathManager.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/services/TinyGopathManager.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.io.exists
 import org.jetbrains.tinygoplugin.configuration.TinyGoConfiguration
 import java.nio.file.Paths
 
@@ -13,9 +14,12 @@ class TinyGopathManager : GoRootsProvider {
     private fun getTinyGoRoot(project: Project, subfolder: String? = null): VirtualFile? {
         val settings = TinyGoConfiguration.getInstance(project)
         val tinyGoSDKPath = settings.tinyGoSDKPath
+        if (tinyGoSDKPath.isEmpty()) {
+            return null
+        }
         val result = if (subfolder == null) Paths.get(tinyGoSDKPath) else Paths.get(tinyGoSDKPath, subfolder)
 
-        return VfsUtil.findFileByIoFile(result.toFile(), true)
+        return if (result.exists()) VfsUtil.findFileByIoFile(result.toFile(), true) else null
     }
 
     override fun getGoPathRoots(project: Project?, p1: Module?): MutableCollection<VirtualFile> {
@@ -30,20 +34,20 @@ class TinyGopathManager : GoRootsProvider {
         }
     }
 
-    override fun getGoPathSourcesRoots(project: Project?, p1: Module?): MutableCollection<VirtualFile> {
+    override fun getGoPathSourcesRoots(project: Project?, module: Module?): Collection<VirtualFile> {
         if (project == null) {
-            return mutableSetOf()
+            return setOf()
         }
         val tinyGoRoot = getTinyGoRoot(project, "src")
         return if (tinyGoRoot == null) {
-            mutableSetOf()
+            setOf()
         } else {
-            mutableSetOf(tinyGoRoot)
+            setOf(tinyGoRoot)
         }
     }
 
-    override fun getGoPathBinRoots(p0: Project?, p1: Module?): MutableCollection<VirtualFile> {
-        return mutableSetOf()
+    override fun getGoPathBinRoots(project: Project?, module: Module?): Collection<VirtualFile> {
+        return setOf()
     }
 
     override fun isExternal(): Boolean = true
