d1de0018fb6afc27b953f13275959010e8205880
Oleksandr Voloshyn <oleksandr.voloshyn@jetbrains.com>
2021-04-19 19:17:49 +0200

    Convert absolute paths for target to relative
    Add a wrapper class for Configuration to convert paths to relative
    and store history/predefined configuration of the project

---
+++ src/main/kotlin/org/jetbrains/tinygoplugin/configuration/ConfigurationWithHistory.kt
@@ -0,0 +1,68 @@
+package org.jetbrains.tinygoplugin.configuration
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.tinygoplugin.sdk.TinyGoSdk
+import org.jetbrains.tinygoplugin.services.tinygoTargets
+
+class SettingsWithHistory(val settings: TinyGoConfiguration, val project: Project) : TinyGoConfiguration by settings {
+    constructor(project: Project) : this(TinyGoConfiguration.getInstance(project).deepCopy(), project)
+
+    override var sdk: TinyGoSdk
+        get() = settings.sdk
+        set(value) {
+            if (value != settings.sdk) {
+                settings.sdk = value
+                predefinedTargets = tinygoTargets(value).toSet()
+            }
+        }
+    override var targetPlatform: String
+        get() {
+            if (predefinedTargets.contains(settings.targetPlatform)) {
+                return settings.targetPlatform
+            }
+            val absolutePath = absoluteOrRelativePath(settings.targetPlatform, project)
+            return absolutePath?.path ?: settings.targetPlatform
+        }
+        set(value) {
+            if (!predefinedTargets.contains(value)) {
+                var relativeTarget = toRelativePath(value, project)
+                if (relativeTarget.isNotEmpty()) {
+                    relativeTarget = value
+                }
+                if (!userTargets.contains(relativeTarget)) {
+                    settings.userTargets = userTargets + relativeTarget
+                }
+            } else {
+                settings.targetPlatform = value
+            }
+        }
+    override var userTargets: List<String>
+        get() = settings.userTargets + predefinedTargets.toList()
+        set(value) {
+            settings.userTargets = value
+        }
+    var predefinedTargets: Set<String> = tinygoTargets(settings.sdk).toSet()
+}
+
+fun absoluteOrRelativePath(path: String, project: Project?): VirtualFile? {
+    val pathUrl = VfsUtil.convertToURL(path) ?: return null
+    var result = VfsUtil.findFileByURL(pathUrl)
+    if (result == null && project != null) {
+        val projectPath = project.basePath
+        val absolutePath = FileUtil.join(projectPath, path)
+        val absoluteUrl = VfsUtil.convertToURL(absolutePath) ?: return null
+        result = VfsUtil.findFileByURL(absoluteUrl)
+    }
+    return result
+}
+
+fun toRelativePath(path: String, project: Project): String {
+    val projectPath = project.basePath ?: return ""
+    if (!path.startsWith(projectPath)) {
+        return ""
+    }
+    return path.substring(projectPath.length)
+}
--- src/main/kotlin/org/jetbrains/tinygoplugin/services/TinyGoSettingsService.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/services/TinyGoSettingsService.kt
@@ -5,49 +5,13 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.NamedConfigurable
+import org.jetbrains.tinygoplugin.configuration.SettingsWithHistory
 import org.jetbrains.tinygoplugin.configuration.TinyGoConfiguration
-import org.jetbrains.tinygoplugin.sdk.TinyGoSdk
 import org.jetbrains.tinygoplugin.ui.ConfigurationProvider
 import org.jetbrains.tinygoplugin.ui.TinyGoPropertiesWrapper
 import org.jetbrains.tinygoplugin.ui.generateSettingsPanel
-import java.io.File
 import javax.swing.JComponent
 
-class SettingsWithHistory(val settings: TinyGoConfiguration) : TinyGoConfiguration by settings {
-    constructor(project: Project) : this(TinyGoConfiguration.getInstance(project).deepCopy())
-
-    override var sdk: TinyGoSdk
-        get() = settings.sdk
-        set(value) {
-            if (value != settings.sdk) {
-                settings.sdk = value
-                predefinedTargets = tinygoTargets(value).toSet()
-            }
-        }
-    override var targetPlatform: String
-        get() = settings.targetPlatform
-        set(value) {
-            if (isTargetValid(value) && !isTargetKnown(value)) {
-                settings.userTargets = userTargets + value
-            }
-            settings.targetPlatform = value
-        }
-    override var userTargets: List<String>
-        get() = settings.userTargets + predefinedTargets
-        set(value) {
-            settings.userTargets = value
-        }
-    var predefinedTargets: Set<String> = tinygoTargets(settings.sdk).toSet()
-    private fun isTargetValid(target: String): Boolean {
-        if (predefinedTargets.contains(target)) return true
-        val jsonTarget = File(target)
-        return jsonTarget.exists() && jsonTarget.isFile
-    }
-
-    private fun isTargetKnown(target: String): Boolean {
-        return predefinedTargets.contains(target) || settings.userTargets.contains(target)
-    }
-}
 
 class TinyGoSettingsService(private val project: Project) :
     NamedConfigurable<TinyGoConfiguration>(), ConfigurationProvider<TinyGoConfiguration> {
