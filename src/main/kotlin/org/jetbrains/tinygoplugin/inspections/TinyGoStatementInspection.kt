4a6317dae78122abb9a69562291d68e2b841c09a
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2021-05-31 11:16:08 +0200

    Suggest to edit settings when 'go' is not supported

--- src/main/kotlin/org/jetbrains/tinygoplugin/inspections/TinyGoStatementInspection.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/inspections/TinyGoStatementInspection.kt
@@ -4,36 +4,29 @@ import com.goide.inspections.core.GoInspectionBase
 import com.goide.inspections.core.GoProblemsHolder
 import com.goide.psi.GoGoStatement
 import com.goide.psi.GoVisitor
-import com.goide.util.GoExecutor
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import org.jetbrains.tinygoplugin.TinyGoBundle
 import org.jetbrains.tinygoplugin.configuration.Scheduler
 import org.jetbrains.tinygoplugin.configuration.TinyGoConfiguration
-import org.jetbrains.tinygoplugin.services.TinyGoInfoExtractor
-import org.jetbrains.tinygoplugin.services.extractTinyGoInfo
-import org.jetbrains.tinygoplugin.services.propagateGoFlags
+import org.jetbrains.tinygoplugin.services.TinyGoSettingsService
 
 class TinyGoStatementInspection : GoInspectionBase() {
     companion object {
-        const val GO_INSPECTION_NAME = "inspection.go.statement.name"
-        const val GO_INSPECTION_MESSAGE = "inspection.go.statement.message"
-        val QUICK_FIX = object : LocalQuickFix {
-            override fun getFamilyName(): String = TinyGoBundle.message(GO_INSPECTION_NAME)
+
+        private val QUICK_FIX = object : LocalQuickFix {
+
+            override fun getFamilyName(): String = TinyGoBundle.message("inspection.go.statement.edit.settings.fix")
 
             override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
-                val settings = TinyGoConfiguration.getInstance(project)
-                settings.scheduler = Scheduler.AUTO_DETECT
-                TinyGoInfoExtractor(project)
-                    .extractTinyGoInfo(settings) { _: GoExecutor.ExecutionResult?, output: String ->
-                        settings.extractTinyGoInfo(output)
-                        settings.saveState(project)
-                        propagateGoFlags(project, settings)
-                        settings.saveState(project)
-                    }
+                ApplicationManager.getApplication().invokeLater {
+                    ShowSettingsUtil.getInstance().editConfigurable(project, TinyGoSettingsService(project))
+                }
             }
         }
     }
@@ -50,7 +43,7 @@ class TinyGoStatementInspection : GoInspectionBase() {
                 if (settings.enabled && settings.scheduler == Scheduler.NONE) {
                     problemsHolder.registerProblem(
                         goStatement,
-                        inspectionMessage(GO_INSPECTION_MESSAGE),
+                        inspectionMessage("inspection.go.statement.message"),
                         ProblemHighlightType.GENERIC_ERROR,
                         QUICK_FIX
                     )
