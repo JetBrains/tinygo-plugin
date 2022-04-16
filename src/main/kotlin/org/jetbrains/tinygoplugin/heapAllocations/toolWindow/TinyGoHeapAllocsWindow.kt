87c4c8e5a18407cf92ecb6ee7c90723c6e08813a
Nikita Mironov <nickfrom22nd@gmail.com>
2022-04-16 13:29:52 +0200

    [GO-11647] Register tool window for heap allocations in plugin.xml

--- src/main/kotlin/org/jetbrains/tinygoplugin/heapAllocations/toolWindow/TinyGoHeapAllocsWindow.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/heapAllocations/toolWindow/TinyGoHeapAllocsWindow.kt
@@ -1,51 +1,43 @@
 package org.jetbrains.tinygoplugin.heapAllocations.toolWindow
 
-import com.intellij.ide.impl.ContentManagerWatcher
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.RegisterToolWindowTask
 import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.content.Content
-import com.intellij.ui.content.ContentManager
+import com.intellij.ui.content.ContentManagerEvent
+import com.intellij.ui.content.ContentManagerListener
 import com.intellij.ui.treeStructure.Tree
-import org.jetbrains.tinygoplugin.TinyGoBundle
 import org.jetbrains.tinygoplugin.heapAllocations.TinyGoHeapAlloc
 import org.jetbrains.tinygoplugin.heapAllocations.toolWindow.model.RootNode
 import org.jetbrains.tinygoplugin.heapAllocations.toolWindow.model.TinyGoHeapAllocsTreeModel
-import org.jetbrains.tinygoplugin.icon.TinyGoPluginIcons
 import java.awt.BorderLayout
 import javax.swing.JPanel
 import javax.swing.tree.TreeSelectionModel
 
-private const val TINYGO_HEAP_ALLOC_TOOLWINDOW_ID = "HeapAlloc"
-private const val TINYGO_HEAP_ALLOC_TOOLWINDOW_TITLE = "heapAllocs.view.title"
+private const val TINYGO_HEAP_ALLOC_TOOLWINDOW_ID = "TinyGo: Heap Allocations"
+
+class TinyGoHeapAllocsToolWindowFactory : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        toolWindow.contentManager.addContentManagerListener(object : ContentManagerListener {
+            override fun contentRemoved(event: ContentManagerEvent) {
+                if (toolWindow.contentManager.contentCount == 0) {
+                    toolWindow.isAvailable = false
+                }
+            }
+        })
+    }
+
+    override fun shouldBeAvailable(project: Project): Boolean = false
+}
 
 @Service
 class TinyGoHeapAllocsViewManager(val project: Project) {
-    private lateinit var contentManager: ContentManager
-
-    init {
-        ApplicationManager.getApplication().invokeLater {
-            val registerToolWindowTask: RegisterToolWindowTask =
-                RegisterToolWindowTask.closableSecondary(
-                    TINYGO_HEAP_ALLOC_TOOLWINDOW_ID,
-                    TinyGoBundle.messagePointer(TINYGO_HEAP_ALLOC_TOOLWINDOW_TITLE),
-                    TinyGoPluginIcons.TinyGoLibraryIcon,
-                    ToolWindowAnchor.RIGHT
-                )
-
-            val toolWindow: ToolWindow = project.service<ToolWindowManager>().registerToolWindow(registerToolWindowTask)
-            contentManager = toolWindow.contentManager
-            ContentManagerWatcher.watchContentManager(toolWindow, contentManager)
-        }
-    }
-
     fun updateHeapAllocsList(update: Map<String, Set<TinyGoHeapAlloc>>) {
         val heapAllocsView = TinyGoHeapAllocsWindow(project)
         heapAllocsView.refreshHeapAllocsList(update)
@@ -57,12 +49,13 @@ class TinyGoHeapAllocsViewManager(val project: Project) {
             val toolWindow: ToolWindow = project.service<ToolWindowManager>()
                 .getToolWindow(TINYGO_HEAP_ALLOC_TOOLWINDOW_ID)
                 ?: return@invokeLater
+            val contentManager = toolWindow.contentManager
 
             contentManager.removeAllContents(true)
-
             val contentTab: Content = contentManager.factory.createContent(heapAllocsView, project.name, false)
             contentManager.addContent(contentTab)
 
+            toolWindow.isAvailable = true
             toolWindow.activate(null, true)
         }
     }
