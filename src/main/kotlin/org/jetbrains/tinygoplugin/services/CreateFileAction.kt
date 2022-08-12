5ec2cb172f68688b503575ebccc9db15b83d1c58
Nikita Mironov <nikita.mironov@jetbrains.com>
2022-08-12 15:52:17 +0200

    [GO-11031] Copy examples that consist of many files

--- src/main/kotlin/org/jetbrains/tinygoplugin/services/CreateFileAction.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/services/CreateFileAction.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.tinygoplugin.services
 
 import com.goide.execution.GoRunUtil
+import com.intellij.icons.AllIcons
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
 import com.intellij.openapi.project.Project
@@ -25,6 +26,10 @@ private const val TINYGO_TEMPLATES_NOT_FOUND_TITLE = "file.create.errors.templat
 private const val TINYGO_TEMPLATES_NOT_FOUND_MESSAGE = "file.create.errors.templatesCannotLoad.message"
 private const val TINYGO_FILE_EXISTS_TITLE = "file.create.errors.fileExists.title"
 private const val TINYGO_FILE_EXISTS_MESSAGE = "file.create.errors.fileExists.message"
+private const val TINYGO_DIR_NOT_EMPTY_TITLE = "file.create.warnings.dirNotEmpty.title"
+private const val TINYGO_DIR_NOT_EMPTY_MESSAGE = "file.create.warnings.dirNotEmpty.message"
+
+private const val GO_MOD_FILENAME = "go.mod"
 
 class CreateFileAction :
     CreateFileFromTemplateAction(
@@ -32,6 +37,7 @@ class CreateFileAction :
         "",
         TinyGoPluginIcons.TinyGoIcon
     ) {
+    private val mainFilesInDirs: MutableMap<VirtualFile, String> = hashMapOf()
 
     override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
         val tinyGoSettings = TinyGoConfiguration.getInstance(project)
@@ -47,32 +53,53 @@ class CreateFileAction :
             }
             builder.setTitle(message(TINYGO_TEMPLATE_DIALOG_NAME))
             val examples = availableExamples(project, tinyGoSettings.sdk.sdkRoot!!)
-            examples.forEach {
+            examples.files.forEach {
                 builder.addKind(it, TinyGoPluginIcons.TinyGoIcon, it)
+            }
+            examples.dirs.sorted().forEach {
+                builder.addKind(it, AllIcons.Nodes.Folder, it)
             }
         }
     }
 
-    private fun availableExamples(project: Project, sdkPath: VirtualFile): Collection<String> {
-        val examples = sdkPath.findChild("src")?.findChild("examples") ?: return emptyList()
+    private fun availableExamples(project: Project, sdkPath: VirtualFile): ExamplesSearchResult {
+        val examples = sdkPath.findChild("src")?.findChild("examples") ?: return ExamplesSearchResult()
         val psiManager = PsiManager.getInstance(project)
-        val result = HashSet<String>()
+        val result = ExamplesSearchResult()
         VfsUtil.visitChildrenRecursively(
             examples,
             object : VirtualFileVisitor<Unit>() {
+                private val ignoredDirectories = hashSetOf<VirtualFile>()
+
                 override fun visitFile(file: VirtualFile): Boolean {
                     if (!file.isDirectory && file.extension == "go") {
                         val psiFile = psiManager.findFile(file)
                         if (GoRunUtil.isMainGoFile(psiFile)) {
-                            result.add(VfsUtil.getRelativePath(file, examples) ?: "")
+                            val noOtherFiles = file.parent.children.size == 1
+                            if (noOtherFiles) {
+                                result.files.addIfNotNull(VfsUtil.getRelativePath(file, examples))
+                            } else {
+                                val parentDir = file.parent
+                                if (parentDir !in ignoredDirectories) {
+                                    mainFilesInDirs[parentDir] = file.name
+                                    ignoredDirectories.add(parentDir.parent)
+                                    result.dirs.addIfNotNull(VfsUtil.getRelativePath(parentDir, examples))
+                                }
+                            }
                         }
                     }
                     return true
                 }
             }
         )
-        return result.filter(String::isNotEmpty)
+        return result
     }
+
+    inner class ExamplesSearchResult {
+        val files: MutableSet<String> = HashSet()
+        val dirs: MutableSet<String> = HashSet()
+    }
+
     @Suppress("ReturnCount")
     override fun createFile(name: String?, templateName: String?, dir: PsiDirectory?): PsiFile? {
         val project = dir?.project ?: return null
@@ -97,12 +124,46 @@ class CreateFileAction :
             )
             return null
         }
-        val result = VfsUtil.copyFile(project, exampleFile, dir.virtualFile, filename)
+        val result = copyExample(project, exampleFile, dir.virtualFile, filename) ?: return null
         val psiManager = PsiManager.getInstance(project)
         return psiManager.findFile(result)
+    }
+
+    @Suppress("ReturnCount")
+    private fun copyExample(
+        project: Project,
+        example: VirtualFile,
+        dir: VirtualFile,
+        filename: String
+    ): VirtualFile? {
+        return if (example.isDirectory) {
+            if (dir.children.any { (it as PsiFile).name != GO_MOD_FILENAME }) {
+                val copyToNonEmptyDir = Messages.showOkCancelDialog(
+                    project,
+                    message(TINYGO_DIR_NOT_EMPTY_MESSAGE),
+                    message(TINYGO_DIR_NOT_EMPTY_TITLE),
+                    Messages.getYesButton(),
+                    Messages.getCancelButton(),
+                    AllIcons.General.Warning
+                )
+                if (copyToNonEmptyDir == Messages.CANCEL) return null
+            }
+
+            VfsUtil.copyDirectory(project, example, dir, null)
+            val mainFileName = mainFilesInDirs[example] ?: return null
+            val mainFile = dir.findChild(mainFileName) ?: return null
+            mainFile.rename(project, filename)
+            mainFile
+        } else {
+            VfsUtil.copyFile(project, example, dir, filename)
+        }
     }
 
     override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String {
         return message(TINYGO_TEMPLATE_ACTION_NAME)
     }
+}
+
+private fun MutableSet<String>.addIfNotNull(element: String?) {
+    if (element != null) add(element)
 }
