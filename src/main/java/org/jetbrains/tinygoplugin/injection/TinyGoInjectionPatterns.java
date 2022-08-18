9171609bae78ce7745c95baff13535e02550ddfe
Nikita Mironov <nikita.mironov@jetbrains.com>
2022-08-18 18:21:48 +0200

    [GO-11648] Apply injection only for supported device platform

--- src/main/java/org/jetbrains/tinygoplugin/injection/TinyGoInjectionPatterns.java
+++ src/main/java/org/jetbrains/tinygoplugin/injection/TinyGoInjectionPatterns.java
@@ -12,35 +12,33 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class TinyGoInjectionPatterns extends PlatformPatterns {
-    public static PsiElementPattern.@NotNull Capture<GoStringLiteral> tinyGoInlineAssembly() {
+    public static PsiElementPattern.@NotNull Capture<GoStringLiteral> tinyGoInlineAssembly(String device) {
         return psiElement(GoStringLiteral.class)
                 .withAncestor(1, psiElement(GoArgumentList.class))
-                .withAncestor(2, asmInjectionFunctionCall());
+                .withAncestor(2, asmInjectionFunctionCall(device));
     }
 
-    private static PsiElementPattern.@NotNull Capture<GoCallExpr> asmInjectionFunctionCall() {
+    private static PsiElementPattern.@NotNull Capture<GoCallExpr> asmInjectionFunctionCall(String device) {
         return psiElement(GoCallExpr.class)
-                .withChild(asmInjectionFunctionReference());
+                .withChild(asmInjectionFunctionReference(device));
     }
 
-    private static PsiElementPattern.@NotNull Capture<GoReferenceExpression> asmInjectionFunctionReference() {
+    private static PsiElementPattern.@NotNull Capture<GoReferenceExpression> asmInjectionFunctionReference(String device) {
         return psiElement(GoReferenceExpression.class)
                 .with(GoCompletionUtil.condition("string value is assembly injection function name", it -> {
                     var identifier = (it).getIdentifier().getText();
                     return identifier.equals("Asm") || identifier.equals("AsmFull");
                 })).withChild(
-                        supportedDeviceLibraryReference()
+                        supportedDeviceLibraryReference(device)
                 );
     }
 
-    private static PsiElementPattern.@NotNull Capture<GoReferenceExpression> supportedDeviceLibraryReference() {
+    private static PsiElementPattern.@NotNull Capture<GoReferenceExpression> supportedDeviceLibraryReference(String device) {
         return new PsiElementPattern.Capture<>(GoReferenceExpression.class) {
             public boolean accepts(@Nullable Object o, ProcessingContext context) {
                 if (o == null) return false;
                 var packageName = ((GoReferenceExpression) o).getText();
-                return packageName.equals("avr")
-                        || packageName.equals("arm")
-                        || packageName.equals("arm64");
+                return packageName.equals(device);
             }
         };
     }
