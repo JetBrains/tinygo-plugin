b5e3c638ffc3abb7a2b5f85b209c03ef4a940cd1
Nikita Mironov <nikita.mironov@jetbrains.com>
2022-09-08 15:39:46 +0200

    [GO-13445] Auto save on running in TinyGo Preview

--- src/main/kotlin/org/jetbrains/tinygoplugin/preview/TinyGoPreviewManager.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/preview/TinyGoPreviewManager.kt
@@ -1,16 +1,20 @@
 package org.jetbrains.tinygoplugin.preview
 
 import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.util.application
 
 class TinyGoPreviewManager(private val editor: TinyGoFileEditorWithPreview) {
     private val preview = editor.previewEditor as TinyGoPreviewEditor
 
     fun compileWasm(onFinish: () -> Unit) {
-        val scratchFile = editor.file ?: return
-        preview.project.service<TinyGoPreviewWasmService>()
-            .compileWasm(scratchFile) {
-                onFinish.invoke()
-            }
+        val file = editor.file ?: return
+        val document = service<FileDocumentManager>().getCachedDocument(file) ?: return
+        application.invokeLater {
+            FileDocumentManager.getInstance().saveDocument(document)
+            preview.project.service<TinyGoPreviewWasmService>()
+                .compileWasm(file) { onFinish.invoke() }
+        }
     }
 
     fun refreshJcef() {
