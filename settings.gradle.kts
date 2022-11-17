fdd7e8396815facd2a32c921c88833ea4a678707
Nikita Mironov <nikita.mironov@jetbrains.com>
2022-11-17 20:44:45 +0100

    Upgrade intellij gradle plugin to 1.10.0

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "tinygo-plugin"
-
-pluginManagement {
-    repositories {
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        gradlePluginPortal()
-    }
-}
