cae52ca1812eaa8220a561d631e19a90393cb6f7
Nikita Mironov <nikita.mironov@jetbrains.com>
2023-06-27 15:28:57 +0200

    Bump versions of kotlin and intellij gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.intellij") version "1.14.2"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
