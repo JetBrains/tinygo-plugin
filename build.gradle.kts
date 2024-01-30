b1e55dcc067c810ae1ce294f6ea17a3acd44f76b
Nikita Mironov <nikita.mironov@jetbrains.com>
2024-01-30 17:43:44 +0100

    Bump Gradle and dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.20"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.16.0"
+    id("org.jetbrains.intellij") version "1.17.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.2.0"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
@@ -119,7 +119,7 @@ tasks {
     }
 
     withType<Test> {
-        systemProperty("idea.home.path", "$buildDir/idea-sandbox/")
+        systemProperty("idea.home.path", "${layout.buildDirectory}/idea-sandbox/")
     }
 
     patchPluginXml {
