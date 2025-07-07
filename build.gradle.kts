bf9dde568a4a8466a2710a018a563d0a779938b2
Nikita Mironov <nikita.mironov@jetbrains.com>
2025-07-07 15:38:33 +0200

    Bump Kotlin language version from 2.1 to 2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.kotlin.jvm") version "2.2.0"
     // IntelliJ Platform Gradle Plugin
     id("org.jetbrains.intellij.platform") version "2.3.0"
     // Gradle Changelog Plugin
