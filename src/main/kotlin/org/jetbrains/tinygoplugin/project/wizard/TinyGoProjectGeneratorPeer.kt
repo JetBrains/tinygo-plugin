017219d6c3e5bd1c06e6c9c3d42b44cfa360e022
Nikita Mironov <Nikita.Mironov@jetbrains.com>
2026-09-08 12:06:30 +0000

    2026.3 EAP compatibility
    Produced by Air Automations. Name: Update TinyGo plugin / Run: https://air.jetbrains.cloud/org/05cf1a7f-6ab5-713b-abd3-29d0c8a05e2d/automations/c145c596-9af7-447a-87c6-9ed9f31b5655?run=59640bce-ed94-44e2-b62d-f535ddc95f4f

--- src/main/kotlin/org/jetbrains/tinygoplugin/project/wizard/TinyGoProjectGeneratorPeer.kt
+++ src/main/kotlin/org/jetbrains/tinygoplugin/project/wizard/TinyGoProjectGeneratorPeer.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.LabeledComponent
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.ui.dsl.builder.panel
 import org.jetbrains.tinygoplugin.configuration.ConfigurationWithHistory
 import org.jetbrains.tinygoplugin.configuration.TinyGoConfiguration
 import org.jetbrains.tinygoplugin.ui.ConfigurationProvider
@@ -22,6 +23,23 @@ class TinyGoProjectGeneratorPeer :
 
     private val propertiesWrapper = TinyGoPropertiesWrapper(this)
 
+    private fun createSettingsPanel(
+        locationComponent: LabeledComponent<TextFieldWithBrowseButton>?,
+        sdkCombo: GoSdkChooserCombo?,
+    ): JPanel =
+        panel {
+            if (locationComponent != null) {
+                row(getLocationComponentLabelText(locationComponent)) {
+                    cell(locationComponent.component).resizableColumn()
+                }
+            }
+            if (sdkCombo != null) {
+                row(SDK_LABEL_TEXT) {
+                    cell(sdkCombo).resizableColumn()
+                }
+            }
+        }
+
     override fun createSettingsPanel(
         parentDisposable: Disposable,
         locationComponent: LabeledComponent<TextFieldWithBrowseButton>?,
@@ -30,14 +48,14 @@ class TinyGoProjectGeneratorPeer :
     ): JPanel {
         val panel = JPanel()
         panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
-        panel.add(createGridPanel(locationComponent, sdkCombo).resize().createPanel())
+        panel.add(createSettingsPanel(locationComponent, sdkCombo))
         panel.add(
             generateTinyGoNewProjectSettingsPanel(
                 project,
                 { locationComponent?.text.orEmpty() },
                 propertiesWrapper,
-                parentDisposable
-            )
+                parentDisposable,
+            ),
         )
         return panel
     }
